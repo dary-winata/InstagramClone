6e94d97f7171b79c115aefdfb062ee8305e7cf3e
Dary winata nugraha djati <71117097+dary-winata@users.noreply.github.com>
2024-10-12 18:59:55 +0700

    first commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by dary winata nugraha djati on 12/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
